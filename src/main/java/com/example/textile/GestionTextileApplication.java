8a71e960866306485c304d0e8557eacccdbee225
TsioriAri <tsioryniainadev@gmail.com>
2026-09-03 10:23:01 +0300

    Initial commit: setup projet Spring Boot (dépendances, architecture, config PostgreSQL)

---
+++ src/main/java/com/example/textile/GestionTextileApplication.java
@@ -0,0 +1,13 @@
+package com.example.textile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTextileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionTextileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/textile/GestionTextileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.textile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTextileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
